300416d878c0cf9db9e515a32da0614e89f657c3
ericpedley@gmail.com <ericpedley@gmail.com>
2020-08-04 09:26:27 -0700

    dnd test and splitting mp3 with code

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 const fetch = require("node-fetch");
 const ffmpeg = require("ffmpeg");
-const { createReadStream } = require('fs');
-const stream = createReadStream('audio-file.flac');
-if(process.env.NODE_ENV!=="production")
+//const { createReadStream } = require('fs');
+//const stream = createReadStream('audio-file.flac');
+if (process.env.NODE_ENV !== "production")
     require("dotenv").config()
-const url = process.env.API_URL
-const options = {
-    method:"POST",
-    headers: {
-        ContentType:"audio/flac",
-        Authorization:`Basic ${Buffer.from(`apikey:${process.env.API_KEY}`,'utf-8').toString("base64")}`
-    },
-    body:stream
-}
-fetch(`${url}/v1/recognize?timestamps=true`,options).then((res)=>{
-    return res.json();
-}).then((json)=>{
-    console.log(JSON.stringify(json.results[0].alternatives));
+// const url = process.env.API_URL
+// const options = {
+//     method: "POST",
+//     headers: {
+//         ContentType: "audio/flac",
+//         Authorization: `Basic ${Buffer.from(`apikey:${process.env.API_KEY}`, 'utf-8').toString("base64")}`
+//     },
+//     body: stream
+// }
+// fetch(`${url}/v1/recognize?timestamps=true`,options).then((res)=>{
+//     return res.json();
+// }).then((json)=>{
+//     console.log(JSON.stringify(json.results[0].alternatives));
+// });
+
+const videoProcess = new ffmpeg("audio-file.mp3");
+videoProcess.then((video) => {
+    video.addCommand("-ss", "00:00:00");
+    video.addCommand("-t","00:00:02");
+    video.save('output.mp3',(error,file)=>{
+        if(!error) 
+            console.log(file);
+        else   
+            console.log(error);
+    });
 });
+
 
 //fmpeg example command https://stackoverflow.com/questions/21491091/splitting-an-audio-mp3-file: ffmpeg -i long.mp3 -acodec copy -ss 00:00:00 -t 00:30:00 half1.mp3
--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
 function App() {
+  const onDragEnd = result => {
+    console.log(result);
+  }
   return (
     <div className="App">
-      Hello world
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="1">
+          {(provided) => (
+          <div 
+            innerRef={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <Draggable draggableId = "0" index="0">
+              {(provided)=> (
+                <div {...provided.draggableProps}{...provided.dragHandleProps} innerRef={provided.innerRef}>
+                  hello world!
+                </div>
+              )}
+            </Draggable>
+            <Draggable draggableId = "0" index="1">
+              {(provided)=> (
+                <div {...provided.draggableProps}{...provided.dragHandleProps} innerRef={provided.innerRef}>
+                  foo bar
+                </div>
+              )}
+            </Draggable>
+
+          </div>)}
+
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
