3590a69cab7c1349f8b1b7a17570102995d1defd
ericpedley@gmail.com <ericpedley@gmail.com>
2020-08-04 10:05:59 -0700

    dnd demo works

--- src/App.js
+++ src/App.js
@@ -7,33 +7,43 @@ function App() {
   return (
     <div className="App">
       <DragDropContext onDragEnd={onDragEnd}>
-        <Droppable droppableId="1">
-          {(provided) => (
-          <div 
-            innerRef={provided.innerRef}
-            {...provided.droppableProps}
-          >
-            <Draggable draggableId = "0" index="0">
-              {(provided)=> (
-                <div {...provided.draggableProps}{...provided.dragHandleProps} innerRef={provided.innerRef}>
-                  hello world!
-                </div>
-              )}
-            </Draggable>
-            <Draggable draggableId = "0" index="1">
-              {(provided)=> (
-                <div {...provided.draggableProps}{...provided.dragHandleProps} innerRef={provided.innerRef}>
-                  foo bar
-                </div>
-              )}
-            </Draggable>
-
-          </div>)}
-
-        </Droppable>
+        <DroppableCard droppableId="droppable-1">
+          <h2>I am a droppable!</h2>
+          <DraggableCard draggableId="draggable-1" index={0}>my custom draggable element 1</DraggableCard>
+          <DraggableCard draggableId="draggable-2" index={1}>my custom draggable element 2</DraggableCard>
+        </DroppableCard>
       </DragDropContext>
-    </div>
+    </div >
   );
+}
+const DroppableCard = (props) => {
+  return (
+    <Droppable droppableId={props.droppableId}>
+      {(provided) => 
+        <div
+          ref={provided.innerRef}
+          {...provided.droppableProps}
+        >
+          {props.children}
+          {provided.placeholder}
+        </div>
+      }
+    </Droppable>
+  )
+}
+
+const DraggableCard = (props) => {
+  return (
+    <Draggable draggableId={props.draggableId} index={props.index}>
+      {(provided) =>
+        <div ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}>
+          <h4>{props.children}</h4>
+        </div>}
+    </Draggable>
+
+  )
 }
 
 export default App;
